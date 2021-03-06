58c77c90d4e8922086b798c50885f237dc8f893b
Grigoriy Mikhalkin <grigoriymikhalkin@gmail.com>
2021-03-06 22:55:03 +0100

    added e2e test

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -1,3 +1,5 @@
+// +build e2e
+
 /*
 Copyright 2020 The Kubernetes Authors.
 
@@ -16,18 +18,145 @@ limitations under the License.
 package e2e
 
 import (
-	"time"
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
 
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/utils/pointer"
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1alpha3"
+	capi_e2e "sigs.k8s.io/cluster-api/test/e2e"
+	"sigs.k8s.io/cluster-api/test/framework"
+	"sigs.k8s.io/cluster-api/test/framework/clusterctl"
+	"sigs.k8s.io/cluster-api/util"
 )
 
-var _ = Describe("functional tests", func() {
-	var ()
+var _ = Describe("Workload cluster creation", func() {
+	var (
+		ctx                 = context.TODO()
+		specName            = "create-workload-cluster"
+		namespace           *corev1.Namespace
+		cancelWatches       context.CancelFunc
+		cluster             *clusterv1.Cluster
+		clusterName         string
+		clusterctlLogFolder string
+	)
 
-	Describe("workload cluster lifecycle", func() {
-		It("It should be creatable and deletable", func() {
-			time.Sleep(5 * time.Second)
-			By("Create cluster")
+	BeforeEach(func() {
+		clusterName = fmt.Sprintf("capp-e2e-%s", util.RandomString(6))
+
+		// Setup a Namespace where to host objects for this spec and create a watcher for the namespace events.
+		namespace, cancelWatches = setupSpecNamespace(ctx, specName, bootstrapClusterProxy, artifactFolder)
+
+		// We need to override clusterctl apply log folder to avoid getting our credentials exposed.
+		clusterctlLogFolder = filepath.Join(os.TempDir(), "clusters", bootstrapClusterProxy.GetName())
+	})
+
+	AfterEach(func() {
+		dumpSpecResourcesAndCleanup(
+			ctx, specName, bootstrapClusterProxy, artifactFolder, namespace, cancelWatches, cluster, e2eConfig.GetIntervals,
+			clusterName, clusterctlLogFolder, skipCleanup)
+	})
+
+	Describe("Should create control-plane and worker", func() {
+		It("Should create a cluster with 1 worker node", func() {
+			By("Initializes with 1 worker node")
+			result := clusterctl.ApplyClusterTemplateAndWait(ctx, clusterctl.ApplyClusterTemplateAndWaitInput{
+				ClusterProxy: bootstrapClusterProxy,
+				ConfigCluster: clusterctl.ConfigClusterInput{
+					LogFolder:                clusterctlLogFolder,
+					ClusterctlConfigPath:     clusterctlConfigPath,
+					KubeconfigPath:           bootstrapClusterProxy.GetKubeconfigPath(),
+					InfrastructureProvider:   clusterctl.DefaultInfrastructureProvider,
+					Flavor:                   clusterctl.DefaultFlavor,
+					Namespace:                namespace.Name,
+					ClusterName:              clusterName,
+					KubernetesVersion:        e2eConfig.GetVariable(capi_e2e.KubernetesVersion),
+					ControlPlaneMachineCount: pointer.Int64Ptr(1),
+					WorkerMachineCount:       pointer.Int64Ptr(1),
+				},
+				WaitForClusterIntervals:      e2eConfig.GetIntervals(specName, "wait-cluster"),
+				WaitForControlPlaneIntervals: e2eConfig.GetIntervals(specName, "wait-control-plane"),
+				WaitForMachineDeployments:    e2eConfig.GetIntervals(specName, "wait-worker-nodes"),
+			})
+
+			cluster = result.Cluster
 		})
 	})
 })
+
+func setupSpecNamespace(
+	ctx context.Context,
+	specName string,
+	clusterProxy framework.ClusterProxy,
+	artifactFolder string,
+) (*corev1.Namespace, context.CancelFunc) {
+	By(fmt.Sprintf("Creating a namespace for hosting the %q test spec", specName))
+	namespace, cancelWatches := framework.CreateNamespaceAndWatchEvents(ctx, framework.CreateNamespaceAndWatchEventsInput{
+		Creator:   bootstrapClusterProxy.GetClient(),
+		ClientSet: bootstrapClusterProxy.GetClientSet(),
+		Name:      fmt.Sprintf("%s-%s", specName, util.RandomString(6)),
+		LogFolder: filepath.Join(artifactFolder, "clusters", bootstrapClusterProxy.GetName()),
+	})
+
+	return namespace, cancelWatches
+}
+
+func dumpSpecResourcesAndCleanup(
+	ctx context.Context,
+	specName string,
+	clusterProxy framework.ClusterProxy,
+	artifactFolder string,
+	namespace *corev1.Namespace,
+	cancelWatches context.CancelFunc,
+	cluster *clusterv1.Cluster,
+	intervalsGetter func(spec, key string) []interface{},
+	clusterName, clusterctlLogFolder string,
+	skipCleanup bool,
+) {
+	// Remove clusterctl apply log folder
+	Expect(os.RemoveAll(clusterctlLogFolder)).ShouldNot(HaveOccurred())
+
+	// Dumps all the resources in the spec namespace, then cleanups the cluster object and the spec namespace itself.
+	By(fmt.Sprintf("Dumping all the Cluster API resources in the %q namespace", namespace.Name))
+	// Dump all Cluster API related resources to artifacts before deleting them.
+	framework.DumpAllResources(ctx, framework.DumpAllResourcesInput{
+		Lister:    bootstrapClusterProxy.GetClient(),
+		Namespace: namespace.Name,
+		LogPath:   filepath.Join(artifactFolder, "clusters", bootstrapClusterProxy.GetName(), "resources"),
+	})
+
+	if !skipCleanup {
+		By(fmt.Sprintf("Deleting cluster %s/%s", cluster.Namespace, cluster.Name))
+		// While https://github.com/kubernetes-sigs/cluster-api/issues/2955 is addressed in future iterations, there is a chance
+		// that cluster variable is not set even if the cluster exists, so we are calling DeleteAllClustersAndWait
+		// instead of DeleteClusterAndWait
+		framework.DeleteAllClustersAndWait(ctx, framework.DeleteAllClustersAndWaitInput{
+			Client:    bootstrapClusterProxy.GetClient(),
+			Namespace: namespace.Name,
+		}, e2eConfig.GetIntervals(specName, "wait-delete-cluster")...)
+
+		By(fmt.Sprintf("Deleting namespace used for hosting the %q test spec", specName))
+		framework.DeleteNamespace(ctx, framework.DeleteNamespaceInput{
+			Deleter: bootstrapClusterProxy.GetClient(),
+			Name:    namespace.Name,
+		})
+
+		// Will call the clean resources just to make sure we clean everything
+		By(fmt.Sprintf("Making sure there is no leftover running for %s", cluster.Name))
+	}
+	cancelWatches()
+	redactLogs()
+}
+
+func redactLogs() {
+	By("Redacting sensitive information from logs")
+	Expect(e2eConfig.Variables).To(HaveKey(RedactLogScriptPath))
+	cmd := exec.Command(e2eConfig.GetVariable(RedactLogScriptPath))
+	cmd.Run()
+}
